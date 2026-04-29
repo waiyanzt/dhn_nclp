0ccf2be88796914a4908008a2a80c91c2ecb6382
Marcus T. <tinw@oregonstate.edu>
2026-04-29 08:09:37 -0700

    bug fix

--- dhn/__init__.py
+++ dhn/__init__.py
@@ -1,4 +1,9 @@
 from .layers import HomConv
 from .models import DHN
-from .datasets import HomDataset, HomDataLoader, hom_collate
-from .graph_enumerations import cycle_mapping_index, clique_mapping_index
+from .datasets import NodeClassDataset, hom_collate
+from .graph_enumerations import (
+    cycle_mapping_index,
+    clique_mapping_index,
+    path_mapping_index,
+    single_node_mapping_index,
+)
